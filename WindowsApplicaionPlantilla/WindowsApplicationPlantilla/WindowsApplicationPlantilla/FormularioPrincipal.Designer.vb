cb4b67b15318a4349ccc18619686ebd4bfa1dadf
osianmicros <osian.moreno@grupmicros.com>
2017-05-17 13:34:45 +0200

    Creando Botones

--- WindowsApplicaionPlantilla/WindowsApplicationPlantilla/WindowsApplicationPlantilla/FormularioPrincipal.Designer.vb
+++ WindowsApplicaionPlantilla/WindowsApplicationPlantilla/WindowsApplicationPlantilla/FormularioPrincipal.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class FormularioPrincipal
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
@@ -22,17 +22,41 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 461)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(120, 44)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Aceptar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(649, 461)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(120, 44)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Salir"
+        Me.Button2.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1432, 262)
+        Me.ClientSize = New System.Drawing.Size(781, 517)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Plantilla Formulario"
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
 
 End Class
--- WindowsApplicaionPlantilla/WindowsApplicationPlantilla/WindowsApplicationPlantilla/My Project/Application.Designer.vb
+++ WindowsApplicaionPlantilla/WindowsApplicationPlantilla/WindowsApplicationPlantilla/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.WindowsApplicationPlantilla.Form1
+            Me.MainForm = Global.WindowsApplicationPlantilla.FormularioPrincipal
         End Sub
     End Class
 End Namespace
